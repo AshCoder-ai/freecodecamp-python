45272322acb6fd45b8869a2c3178c8a7c53e812b
Ashish Prashar <227779999+AshCoder-ai@users.noreply.github.com>
2026-06-21 22:02:05 +0530

    added update_setting and delete_setting function

--- user_configuration_manager/main.py
+++ user_configuration_manager/main.py
@@ -12,4 +12,26 @@ def add_setting(setting_dict, kv_tuple):
     else:
         setting_dict[k] = v
         return f"Setting '{k}' added with value '{v}' successfully!"
+        
+def update_setting(setting_dict, kv_tuple):
+    k, v = kv_tuple
+    k = k.lower()
+    v = v.lower()
 
+    for keys in setting_dict.keys():
+        if k == keys.lower():
+            setting_dict[keys] = v
+            return f"Setting '{k}' updated to '{v}' successfully!"
+    else:
+        return f"Setting '{k}' does not exist! Cannot update a non-existing setting."
+
+
+def delete_setting(setting_dict, key):
+    key = key.lower()
+
+    for k in setting_dict.keys():
+        if key == k.lower():
+            setting_dict.pop(k)
+            return f"Setting '{key}' deleted successfully!"
+    else:
+        return "Setting not found!"
